8d355b1999998d84be7f3c3f50e15aceef907a2e
Simon Hensel <simon.hensel@protonmail.com>
2025-10-15 17:58:07 +0200

    Simplify module to only support realm time offsets

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # mod-TimeIsTime
-TimeIsTime is a module for Azerothcore to adjust the day/night cycle speed in World of Warcraft 3.3.5a
+Simplified version of [mod-TimeIsTime](https://github.com/dunjeon/mod-TimeIsTime) that allows to set a realm time offset without the need to set epoch timestamps.
+Now you can change the realm time without having to change your system clock each time you start the server.
 
-# Settings
-- Default:  1.0 (Blizzlike)
-- 60.0  = 1 Game Day every 24   minutes
-- 30.0  = 1 Game Day every 48   minutes
-- 15.0  = 1 Game Day every 96   minutes or 1.6  Hours
-- 7.5   = 1 Game Day every 192  minutes or 3.2  Hours
-- 3.75  = 1 Game Day every 384  minutes or 6.4  Hours
-- 1.875 = 1 Game Day every 768  minutes or 12.8 Hours
-- 1.0   = 1 Game Day every 1440 minutes or 24.0 Hours 
+Refer to `mod-time_is_time.conf.dist` for all config options.
--- src/TimeIsTime.cpp
+++ src/TimeIsTime.cpp
@@ -14,9 +14,7 @@
 
 static bool   stimeistime_enable,
               stimeistime_announce;
-static float  stimeistime_speed_rate,
-              stimeistime_hour_offset;
-static uint32 stimeistime_time_start;
+static uint32 stimeistime_hour_offset;
 
 class TimeIsTimeBeforeConfigLoad : public WorldScript {
 public:
@@ -26,9 +24,7 @@ public:
     void OnBeforeConfigLoad(bool /*reload*/) override {
         stimeistime_enable = sConfigMgr->GetOption<bool>("TimeIsTime.Enable", true);
         stimeistime_announce = sConfigMgr->GetOption<bool>("TimeIsTime.Announce", true);
-        stimeistime_speed_rate = sConfigMgr->GetOption<float>("TimeIsTime.SpeedRate", 1.0);
-        stimeistime_hour_offset = sConfigMgr->GetOption<float>("TimeIsTime.HourOffset", 0.0);
-        stimeistime_time_start = sConfigMgr->GetOption<int>("TimeIsTime.TimeStart", 0);
+        stimeistime_hour_offset = sConfigMgr->GetOption<int>("TimeIsTime.HourOffset", 0);
     }
 };
 
@@ -41,23 +37,16 @@ public:
         if (stimeistime_enable && stimeistime_announce)
             ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00TimeIsTime |rmodule");
     }
-	
+
     void OnPlayerSendInitialPacketsBeforeAddToMap(Player* player, WorldPacket& data) override {
         if (!stimeistime_enable)
             return;
 
-        uint32 speed_time  = (GameTime::GetGameTime().count() - GameTime::GetUptime().count()) + (GameTime::GetUptime().count() * stimeistime_speed_rate);
-        float  hour_offset = stimeistime_hour_offset  * 3600;
-        uint32 time_start  = stimeistime_time_start + hour_offset;
+        uint32 hour_offset = GameTime::GetGameTime().count() + stimeistime_hour_offset * 3600;
 
         data.Initialize(SMSG_LOGIN_SETTIMESPEED, 4 + 4 + 4);
-        if (stimeistime_time_start > 0) {
-            data.AppendPackedTime(time_start);
-        }
-        else {
-            data.AppendPackedTime(speed_time);
-        }
-        data << float(0.01666667f) * stimeistime_speed_rate;
+        data.AppendPackedTime(hour_offset);
+        data << float(0.01666667f);
         data << uint32(0);
 
         player->GetSession()->SendPacket(&data);
